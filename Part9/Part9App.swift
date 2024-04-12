e22f0705e232a2419a1aa2b9bc8aa6e36cef29d9
nki1121 <nkiuniv@gmail.com>
2024-04-12 14:11:57 +0900

    Initial Commit

---
+++ Part9/Part9App.swift
@@ -0,0 +1,17 @@
+//
+//  Part9App.swift
+//  Part9
+//
+//  Created by 中井晋平 on 2024/04/12.
+//
+
+import SwiftUI
+
+@main
+struct Part9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
